c6d69bf59ce9c53e3024ebdd5f5c9fb2610cb140
akyeshi <akyeshi@outlook.com>
2024-05-13 18:32:27 -0400

    product model created

---
+++ models/product.js
@@ -0,0 +1,35 @@
+
+
+
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+// define user schema and add required fields/validation properties on the user schema/model 
+const productSchema = new Schema({
+  title: {
+    type: String, 
+    required: true
+  }, 
+  description: {
+    type: String, 
+    trim: true,     // Mongoose trim whitespaces before and after the string before saving 
+    required: true 
+  }, 
+  price: Number, 
+  quantity: Number, 
+  images: [String], 
+  category: {
+    type: Schema.Types.ObjectId, 
+    ref: 'Category', 
+    required: true 
+  }, 
+  reviews: [reviewSchema], 
+  tags: [String]
+}, {
+  timestamps: true, 
+}); 
+
+
+// compile the product schema into a product model and export it 
+module.exports = mongoose.model('Product', productSchema); 
+
